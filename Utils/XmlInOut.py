0a37ea00c3fca00c019563026e91bb18d747219b
Mezer_Ilya <mr.mezer@myttk.ru>
2022-09-10 14:58:11 +0700

    Импорт готов в XML доработан.

--- Utils/XmlInOut.py
+++ Utils/XmlInOut.py
@@ -1,18 +1,17 @@
-
 import dicttoxml
 import xmltodict
 
-data = {'name1': '111',
-        'name2': '222',
-        'name3': '333',
-        'name4': '444',
-        'name5': '555'}
 
-def Out(filename, data):
-    raw_data = dicttoxml.dicttoxml(data, attr_type = False, custom_root='phone_book')
+def Import(filename):
+    xml_data = open(filename).read()
+    data = xmltodict.parse(xml_data)
+    return data
+
+def Export(filename, data):
+    raw_data = dicttoxml.dicttoxml(data, attr_type=False, custom_root='phone_book')
     str_data = raw_data.decode()
     with open(filename, "w") as f:
         f.write(str_data)
 
 
-Out('1.xml', data)
+
